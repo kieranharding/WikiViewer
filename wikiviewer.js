d798a41b51f254c839a9b7355ad463d52c916a49
Kieran Harding <kieranharding@gmail.com>
2016-08-30 05:57:51 -0600

    First working commit
    Setup to load and run off Codepen (i.e. all external resources load correctly). Still no CSS.

--- wikiviewer.js
+++ wikiviewer.js
@@ -1,6 +1,42 @@
-$(
-  var $results = $("#results")
-  var baseStr = "https://en.wikipedia.org/w/api.php"
+function displayResults(data) {
+  var titles = data[1]
+  var extracts = data[2]
+  var links = data[3]
+  titles.map(function (res, idx) {
+    $my.results.append('<p class="result-title">' + res + '</p>')
+      .append('<a href="' + links[idx] + '" class="result-link">' + links[idx] + '</a>')
+      .append('<p class="result-snippet">' + extracts[idx] + '</p>')
+  })
+}
+
+function submitSearch() {
+  $my.results.empty()
+  var query = $("#query").val()
+  if (query) {
+    $.ajax({
+      url: $my.baseStr,
+      jsonp: "callback",
+      dataType: "jsonp",
+      data: {
+        action: "opensearch",
+        format: "json",
+        profile: "classic",
+        limit: "25",
+        search: query
+      },
+      xhrFields: {
+        withCredentials: true
+      },
+      success: displayResults
+    })
+  }
+}
+
+$(function() {
+  window.$my = {
+    results: $("#results"),
+    baseStr: "https://en.wikipedia.org/w/api.php"
+  }
 
   $("#search").click(function(e) {
     submitSearch()
@@ -9,38 +45,4 @@ $(
   $("#query").keypress(function(e) {
     if (e.which == 13) submitSearch()
   })
-
-  function displayResults(data) {
-    var titles = data[1]
-    var extracts = data[2]
-    var links = data[3]
-    titles.map(function (res, idx) {
-      $results.append('<p class="result-title">' + res + '</p>')
-        .append('<a href="' + links[idx] + '" class="result-link">' + links[idx] + '</a>')
-        .append('<p class="result-snippet">' + extracts[idx] + '</p>')
-    })
-  }
-
-  function submitSearch() {
-    $results.empty()
-    var query = $("#query").val()
-    if (query) {
-      $.ajax({
-        url: baseStr,
-        jsonp: "callback",
-        dataType: "jsonp",
-        data: {
-          action: "opensearch",
-          format: "json",
-          profile: "classic",
-          limit: "25",
-          search: query
-        },
-        xhrFields: {
-          withCredentials: true
-        },
-        success: displayResults
-      })
-    }
-  }
-)
+})
